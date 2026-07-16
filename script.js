d1c2caad925b2ae6fbbfcdb8d2f4b282f41a4b43
ArthurZelo22 <arthur_t_souza@estudante.sesisenai.org.br>
2026-07-16 12:35:44 -0300

    Bruno começou o JS

---
+++ script.js
@@ -0,0 +1,20 @@
+function botaoCaixa(){
+    
+    let containerCarrinho = document.getElementById('carrinho')
+    let pCarrinho = document.getElementById('carrinho-vazio')
+
+    if (pCarrinho && pCarrinho.innerText === 'Seu carrinho está vazio'){
+        pCarrinho.remove()}
+
+
+    let listaItens = document.createElement('div')
+    containerCarrinho.appendChild(listaItens)
+
+
+    let novoItem = document.createElement('div')
+    novoItem.id = 'item-no-carrinho'
+
+    novoItem.innerHTML = ('Caixa de som Bluetooth   R$ 750,00   <button onclick="this.parentElement.remove()">Remover</button>')
+
+    listaItens.appendChild(novoItem)
+    }
