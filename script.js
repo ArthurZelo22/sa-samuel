e925553f99d31f11dd5d251d0a042567c86c6547
ArthurZelo22 <arthur_t_souza@estudante.sesisenai.org.br>
2026-07-16 12:38:52 -0300

    Botões adicionados

--- script.js
+++ script.js
@@ -18,3 +18,49 @@ function botaoCaixa(){
 
     listaItens.appendChild(novoItem)
     }
+
+
+
+function botaoFone(){
+    
+    let containerCarrinho = document.getElementById('carrinho')
+    let pCarrinho = document.getElementById('carrinho-vazio')
+
+    if (pCarrinho && pCarrinho.innerText === 'Seu carrinho está vazio'){
+        pCarrinho.remove()}
+
+
+    let listaItens = document.createElement('div')
+    containerCarrinho.appendChild(listaItens)
+
+
+    let novoItem = document.createElement('div')
+    novoItem.id = 'item-no-carrinho'
+
+    novoItem.innerHTML = ('Fone de ouvido Bluetooth   R$ 250,00   <button onclick="this.parentElement.remove()">Remover</button>')
+
+    listaItens.appendChild(novoItem)
+    }
+
+
+
+function botaoRelogio(){
+    
+    let containerCarrinho = document.getElementById('carrinho')
+    let pCarrinho = document.getElementById('carrinho-vazio')
+
+    if (pCarrinho && pCarrinho.innerText === 'Seu carrinho está vazio'){
+        pCarrinho.remove()}
+
+
+    let listaItens = document.createElement('div')
+    containerCarrinho.appendChild(listaItens)
+
+
+    let novoItem = document.createElement('div')
+    novoItem.id = 'item-no-carrinho'
+
+    novoItem.innerHTML = ('Relógio   R$ 550,00   <button onclick="this.parentElement.remove()">Remover</button>')
+
+    listaItens.appendChild(novoItem)
+    }
