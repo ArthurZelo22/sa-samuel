89f5def07121100191b81c3ef4f26f9f0e344326
ArthurZelo22 <arthur_t_souza@estudante.sesisenai.org.br>
2026-07-23 21:07:01 -0300

    páginas completas, só faltam alguns detalhes

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let contador = 0
+let valorSubtotal = 0
+let valorTotal = 0
 
 function botaoCaixa() {
 
     let containerCarrinho = document.getElementById('carrinho')
     let pCarrinho = document.getElementById('carrinho-vazio')
+    let subtotal = document.getElementById('pSubtotal')
+    let total = document.getElementById('pTotal')
+    let pDesconto = document.getElementById('desconto')
 
     if (pCarrinho && pCarrinho.innerText === 'Seu carrinho está vazio') {
         pCarrinho.remove()
@@ -15,13 +20,26 @@ function botaoCaixa() {
 
     listaItens.id = 'lista-itens'
     let novoItem = document.createElement('div')
-    novoItem.className = 'item-no-carrinho'
+    novoItem.className = 'item-no-carrinho-caixa'
 
     novoItem.innerHTML = ('Caixa de som Bluetooth |  R$ 750,00   <button onclick="removerItem(this)">Remover</button>')
 
     listaItens.appendChild(novoItem)
 
     contador++
+
+    valorSubtotal += 750
+    pSubtotal.innerText = `${(valorSubtotal).toFixed(2)}`
+
+    valorTotal = valorSubtotal
+
+    if (valorSubtotal > 1000){
+        valorTotal = valorSubtotal * 0.9
+
+        pDesconto.innerText = 'Desconto de 10% para compras acima de R$ 1.000,00 aplicado!'
+    }
+
+    pTotal.innerText = `${(valorTotal).toFixed(2)}`
 }
 
 
@@ -30,6 +48,9 @@ function botaoFone() {
 
     let containerCarrinho = document.getElementById('carrinho')
     let pCarrinho = document.getElementById('carrinho-vazio')
+    let subtotal = document.getElementById('pSubtotal')
+    let total = document.getElementById('pTotal')
+    let pDesconto = document.getElementById('desconto')
 
     if (pCarrinho && pCarrinho.innerText === 'Seu carrinho está vazio') {
         pCarrinho.remove()
@@ -41,13 +62,25 @@ function botaoFone() {
 
 
     let novoItem = document.createElement('div')
-    novoItem.className = 'item-no-carrinho'
+    novoItem.className = 'item-no-carrinho-fone'
 
     novoItem.innerHTML = ('Fone de ouvido Bluetooth  | R$ 250,00   <button onclick="removerItem(this)">Remover</button>')
 
     listaItens.appendChild(novoItem)
 
     contador++
+
+    valorSubtotal += 250
+    pSubtotal.innerText = `${(valorSubtotal).toFixed(2)}`
+
+    valorTotal = valorSubtotal
+
+    if (valorSubtotal > 1000){
+        valorTotal = valorSubtotal * 0.9
+        pDesconto.innerText = 'Desconto de 10% para compras acima de R$ 1.000,00 aplicado!'
+    }
+
+    pTotal.innerText = `${(valorTotal).toFixed(2)}`
 }
 
 
@@ -56,6 +89,9 @@ function botaoRelogio() {
 
     let containerCarrinho = document.getElementById('carrinho')
     let pCarrinho = document.getElementById('carrinho-vazio')
+    let subtotal = document.getElementById('pSubtotal')
+    let total = document.getElementById('pTotal')
+    let pDesconto = document.getElementById('desconto')
 
     if (pCarrinho && pCarrinho.innerText === 'Seu carrinho está vazio') {
         pCarrinho.remove()
@@ -67,29 +103,109 @@ function botaoRelogio() {
 
 
     let novoItem = document.createElement('div')
-    novoItem.className = 'item-no-carrinho'
+    novoItem.className = 'item-no-carrinho-relogio'
 
     novoItem.innerHTML = ('Relógio |  R$ 550,00   <button onclick="removerItem(this)">Remover</button>')
 
     listaItens.appendChild(novoItem)
 
     contador++
+
+    valorSubtotal += 550
+    pSubtotal.innerText = `${(valorSubtotal).toFixed(2)}`
+
+    valorTotal = valorSubtotal
+
+    if (valorSubtotal > 1000){
+        valorTotal = valorSubtotal * 0.9
+        pDesconto.innerText = 'Desconto de 10% para compras acima de R$ 1.000,00 aplicado!'
+    }
+
+    pTotal.innerText = `${(valorTotal).toFixed(2)}`
 }
 
 
 
 function removerItem(botao) {
     let containerCarrinho = document.getElementById('carrinho')
+    let pDesconto = document.getElementById('desconto')
 
-    botao.parentElement.remove()
-    contador--
+    if (botao.parentElement.className === 'item-no-carrinho-caixa') {
+
+        valorSubtotal -= 750
+
+        botao.parentElement.remove()
+        contador--
+
+        pSubtotal.innerText = `${(valorSubtotal).toFixed(2)}`
+
+        valorTotal = valorSubtotal
+
+        if (valorSubtotal > 1000){
+        valorTotal = valorSubtotal * 0.9
+        pDesconto.innerText = 'Desconto de 10% para compras acima de R$ 1.000,00 aplicado!'
+        }
+
+        else{
+            pDesconto.innerText = 'Ganhe 10% de desconto em compras acima de R$ 1.000,00!'
+        }
+
+        pTotal.innerText = `${(valorTotal).toFixed(2)}`
+    }
+
+    else if (botao.parentElement.className === 'item-no-carrinho-fone') {
+
+        valorSubtotal -= 250
+
+        botao.parentElement.remove()
+        contador--
+
+        pSubtotal.innerText = `${(valorSubtotal).toFixed(2)}`
+
+        valorTotal = valorSubtotal
+
+        if (valorSubtotal > 1000){
+        valorTotal = valorSubtotal * 0.9
+        pDesconto.innerText = 'Desconto de 10% para compras acima de R$ 1.000,00 aplicado!'
+        }
+
+        else{
+            pDesconto.innerText = 'Ganhe 10% de desconto em compras acima de R$ 1.000,00!'
+        }
+
+        pTotal.innerText = `${(valorTotal).toFixed(2)}`
+    }
+
+    else if (botao.parentElement.className === 'item-no-carrinho-relogio') {
+
+        valorSubtotal -= 550
+
+        botao.parentElement.remove()
+        contador--
+
+        pSubtotal.innerText = `${(valorSubtotal).toFixed(2)}`
+
+        valorTotal = valorSubtotal
+
+        if (valorSubtotal > 1000){
+        valorTotal = valorSubtotal * 0.9
+        pDesconto.innerText = 'Desconto de 10% para compras acima de R$ 1.000,00 aplicado!'
+        }
+
+        else{
+            pDesconto.innerText = 'Ganhe 10% de desconto em compras acima de R$ 1.000,00!'
+        }
+
+        pTotal.innerText = `${(valorTotal).toFixed(2)}`
+    }
+
 
     if (contador === 0) {
         let pCarrinho = document.createElement('p')
         pCarrinho.id = 'carrinho-vazio'
         pCarrinho.innerText = 'Seu carrinho está vazio'
         containerCarrinho.appendChild(pCarrinho)
-    }
+    }   
 }
 
 function aparecerHistoria() {
